80f71d48a6abbfe360467a7499e19014f4e5a3db
ginehann <66596817+ginehann@users.noreply.github.com>
2023-05-17 04:19:11 +0800

    Update GraphUtils.py

--- GraphUtils.py
+++ GraphUtils.py
@@ -3,14 +3,18 @@ import pandas as pd
 
 def generate_graph_and_mean(table, datetime, output_graph):
     df = pd.read_csv(table)
-
+    
+    # plot a graph with x= time, y= aggregate velocity
     plt.plot(df['Time'], df['Aggregate Velocity'], color='red', label='Velocity (Rolling)')
     plt.legend()
     plt.xlabel('Time (seconds)')
     plt.ylabel('Velocity (pixels per second)')
     plt.title(f"Fishes' velocity when fed at {datetime}")
     plt.ylim(None, 1500)
+    
+    # save it into a file
     plt.savefig(output_graph) 
 
+    # also return mean velocity
     mean_activity = df['Aggregate Velocity'].mean()
     return mean_activity 

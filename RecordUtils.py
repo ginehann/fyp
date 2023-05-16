a77c185a5ccf1a7e46e5d9d26dbfd5f885377276
ginehann <66596817+ginehann@users.noreply.github.com>
2023-05-17 04:22:34 +0800

    Update RecordUtils.py

--- RecordUtils.py
+++ RecordUtils.py
@@ -1,14 +1,19 @@
 import os
 import subprocess
 
-def record_stream(stream_url, datetime, cwd, duration):
+
+def record_stream(stream_url, datetime, cwd, duration): # cwd = file directory
     resolution = "640x480"  # Set resolution to 640x480
     
+    # create file with current datetime as name
     if not os.path.isdir(f"{cwd}/recordings/{datetime}"):
         os.makedirs(f"{cwd}/recordings/{datetime}")
     
     print("Recording stream...")
-    # Run ffmpeg command to record stream with specified settings
+    # Run ffmpeg command to record stream...
     subprocess.run(["ffmpeg", "-i", stream_url, "-t", duration, "-s", resolution, "-c", "copy", "-an", f"{cwd}/recordings/{datetime}/{datetime}-s.mp4"])
-    subprocess.run(["ffmpeg", "-i", f"{cwd}/recordings/{datetime}/{datetime}-s.mp4", "-vf", "fps=15.83", f"{cwd}/recordings/{datetime}/{datetime}.mp4"])
+    # then process it so that it has specified setting and runs in real time.
+    subprocess.run(["ffmpeg", "-i", f"{cwd}/recordings/{datetime}/{datetime}-s.mp4", "-ss", "0:00:00", "-t", "00:02:00", "-vf", "fps=15.83,setpts=1.25*PTS", f"{cwd}/recordings/{datetime}/{datetime}.mp4"])
     print("Recording done.")
+
+    
